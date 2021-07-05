6c61fb3c79d3b680edafc651f603e7a73e82ca71
Dipankar Maurya <dipankarmaurya5855@gmail.com>
2021-07-05 15:49:48 +0530

    setting of front for todo list and install mongoose

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const port = 4444;
-
+const path =require('path');
 
 const app =express();
+// seting ejs view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+//serving static files
+app.use(express.static('./assets'));
+// seting routes
 app.use('/',require('./routes'));
 app.listen(port,(err)=>{
  if(err){
